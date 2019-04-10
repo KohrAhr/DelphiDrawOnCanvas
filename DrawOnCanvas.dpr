a91625660e94d1c1bfbbe5cca0de5616ed411cdb
ZAM <ZAM@1CLICK.LV>
2019-04-10 10:21:22 +0300

    Initial commit

---
+++ DrawOnCanvas.dpr
@@ -0,0 +1,14 @@
+program DrawOnCanvas;
+
+uses
+  Vcl.Forms,
+  ufmMain in 'ufmMain.pas' {fmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfmMain, fmMain);
+  Application.Run;
+end.
